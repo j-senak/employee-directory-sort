7a21c838d82b83e8ac01faa56397f7f61f71a7c2
JohnS <senakj2011@hotmail.com>
2020-05-01 01:27:24 -0400

    set up Components folder

---
+++ src/components/EmployeeDirectory/List.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Item from "./Item";
+
+const List = () => {
+    return (
+        <div>
+            This is my list component.
+            <Item/>
+        </div>
+    );
+};
+
+export default List;
--- src/containers/EmployeeDirectory.jsx
+++ src/containers/EmployeeDirectory.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import List from "../components/EmployeeDirectory/List"
 
 class EmployeeDirectory extends Component {
     render() {
         return (
             <div>
                 <h1>Welcome to the employee directory</h1>
+                <List/>
             </div>
         );
     }
