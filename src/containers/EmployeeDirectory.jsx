34e64b625a03d6a833a1f736facc75b89d7f05de
JohnS <senakj2011@hotmail.com>
2020-05-01 01:38:21 -0400

    Added employee array

--- src/containers/EmployeeDirectory.jsx
+++ src/containers/EmployeeDirectory.jsx
@@ -2,11 +2,70 @@ import React, { Component } from 'react';
 import List from "../components/EmployeeDirectory/List"
 
 class EmployeeDirectory extends Component {
+    state = {
+        employees:
+        [
+            {
+              id: "1",
+              name: "Jessica Triscut",
+              position: "Manager",
+              phone: "111-111-1111"
+            },
+            {
+              id: "2",
+              name: "Ryan Biscuit",
+              position: "Coder",
+              phone: "222-222-2222"
+            },
+            {
+              id: "3",
+              name: "Sandy Cracker",
+              position: "Tester",
+              phone: "333-333-3333"
+            },
+            {
+              id: "4",
+              name: "Randy Cake",
+              position: "Coder",
+              phone: "444-444-4444"
+            },
+            {
+              id: "5",
+              name: "Brian Crumble",
+              position: "Manager",
+              phone: "555-555-5555"
+            },
+            {
+              id: "6",
+              name: "Sophia Brulee",
+              position: "Coder",
+              phone: "666-666-6666"
+            },
+            {
+              id: "7",
+              name: "Piere Croquembouche",
+              position: "Tester",
+              phone: "777-777-7777"
+            },
+            {
+              id: "8",
+              name: "Violet Macaron",
+              position: "Tester",
+              phone: "888-888-8888"
+            },
+            {
+              id: "9",
+              name: "Terry Tart",
+              position: "Coder",
+              phone: "999-999-9999"
+            }
+          ]
+    }
     render() {
         return (
             <div>
                 <h1>Welcome to the employee directory</h1>
-                <List/>
+                <List employees={this.state.employees}/>
             </div>
         );
     }
